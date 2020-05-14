d11a09c5d2916315dcd99b16833cfdbbc63faf2b
LeRoy <sourcecodej52@gmail.com>
2020-05-14 21:51:47 +0200

    first init with client.php. client.php class processes user data

---
+++ client.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'login.php';
+require_once 'connect.php';
+    
+//class User holds the functionality of returning and processing basic userdetails such as name, surname, age and gender
+class Person
+{
+    private $name = null;
+    private $surname = null;
+    private $dob = null;
+    private $gender = null;
+
+    function __construct($name, $surname, $dob, $gender)
+    {
+        $this -> name = $name;
+        $this -> surname = $surname;
+        $this -> gender = $gender;
+        $this -> dob = $dob;
+    }
+    //getters for private properties
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getSurname()
+    {
+        return $this -> surname;
+    }
+
+    public function getDob()
+    {
+        return $this -> dob;
+    }
+
+    public function getGender()
+    {
+        return $this -> gender;
+    }
+}
+?>
---
+++ processShoppingCart.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//Process shopping cart is invoked when the user has decided to check out the items
+//
+class ProcessShoppingCart
+{
+    
+}
