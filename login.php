9d0fb8f1a971ba3f2f6ee08d03a83489b2dbe7de
LeRoy <sourcecodej52@gmail.com>
2020-05-22 17:03:46 +0200

    login.php hold propertis for connecting to the database

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$db_hostname = "localhost";
+$db_username = "leRoy";
+$db_password = "leroypass";
+$db_database = "online_store";
+
+?>
+
