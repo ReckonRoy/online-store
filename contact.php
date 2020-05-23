333952844594e21db00d763c49824f6e6a469ef5
LeRoy <sourcecodej52@gmail.com>
2020-05-23 18:37:25 +0200

    added a constructor to the contact class. that  initialises the the private properties

--- contact.php
+++ contact.php
@@ -11,6 +11,34 @@
  *
  * @author le-roy
  */
-class contact {
-    //put your code here
+class contact
+{
+    private $name;
+    private $email;
+    private $address;
+    private $message;
+    
+    function __construct($name, $email, $address, $message)
+    {
+        $this -> name = $name;
+        $this -> email = $email;
+        $this -> address = $address;
+        $this -> message = $message;
+        
+    }
 }
+
+?>
+
+<html>
+<body>
+    
+    <form action="contact.php" method="post">
+        <input type="text" name="name" placeholder="Name...">
+        <input type="text" name="name" placeholder="Email...">
+        <input type="text" name="name" placeholder="Address...">
+        <textarea cols="20" rows="10" name="message">
+    </form>
+    
+</body>
+</html>
