e521d5002a588f863ebfb91c4cdb96dc7fcebcb8
LeRoy <sourcecodej52@gmail.com>
2020-05-25 19:34:21 +0200

    modified accountLogin - When user is succesfuly logged in a session is created. page communicates with a javascript file through ajax

--- accountLogin.php
+++ accountLogin.php
@@ -11,35 +11,30 @@
  *
  * @author le-roy
  */
+require_once 'login.php';
+$connection = new mysqli($db_hostname, $db_username, $db_password, $db_database);
 
-    if(isset($_POST['username']) && isset($_POST['password']))
+if(isset($_POST['username']) && isset($_POST['password']))
+{
+    if(!empty($_POST['username']) && !empty($_POST['password']))
     {
-        if(!empty($_POST['username']) && !empty($_POST['password']))
-        {
-            $username = $_POST['username'];
-            $password = $_POST['password'];
-            
-            
-        }
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        
+        $loginObject = new accountLogin($username, $password);
+        $loginObject -> login($connection);
     }
+}
 class accountLogin
 {
-    private $username;
-    private $password;
+    private $username = "";
+    private $password = "";
     function __construct($username, $password)
     {
         
         $this -> username = $username;
         $this -> password = $password;
         
-    }
-    
-    public function login($conn)
-    {
-        if($conn -> connect_error)
-        {
-            exit("Sorry we could not establish a connection to the database");
-        }
     }
     
     public function getUsername()
@@ -50,6 +45,54 @@ class accountLogin
     {
         return $this -> password;
     }
+    
+    public function login($conn)
+    {
+        if($conn -> connect_error)
+        {
+            exit("Sorry we could not establish a connection to the database");
+        }
+        
+        $query = "SELECT id FROM client_details WHERE username = '".$this->getUsername()."' AND password = '".$this->getPassword()."'";
+        $result = $conn -> query($query);
+        if($result)
+        {
+            if($result -> num_rows != 0)
+            {
+                $row_id = $result -> fetch_assoc();
+                $id = $row_id['id'];
+                
+                //get name and last name belonging to this id
+                $query = "SELECT name, surname FROM client WHERE id = '".$id."'";
+                $result = $conn->query($query);
+                if($result)
+                {
+                    if($result -> num_rows != 0)
+                    {
+                        while($each_row = $result -> fetch_assoc())
+                        {
+                            $name = $each_row['name'];
+                            $surname = $each_row['surname'];
+                            
+                            session_start();
+                            $_SESSION[$name];
+                            $_SESSION[$surname];
+                        }
+                    }else{
+                        echo 'technical error';
+                    }
+                } else {
+                    echo "There seem to be an error with the connection";
+                }
+            }else{
+                echo 'invalid username and password combination';
+            }
+        } else {
+            echo 'mysql query error';
+        }
+    }
+    
+    
 }
 
 ?>
@@ -99,6 +142,14 @@ class accountLogin
             padding: 5px;
             width: 100px;
         }
+        
+        #misc
+        {
+            width: 90%;
+            margin: 0px auto;
+            margin-top: 80px;
+            border: 1px solid black;
+        }
     </style>
     <body>
         <div class="aside">
@@ -114,7 +165,11 @@ class accountLogin
                 <div id="btn_l_field">
                     <input type="submit" value="LOGIN">
                 </div>
-            </form
+                <div id="misc">
+                    <center><p>Don't have an account? <span><a href="register.html">Sign Up</a></span></p></center>
+                    <center><p><a href="recovery.php">Forgot your password?</a></p></center>
+                </div>
+            </form>
         </div>
     </body>
 </html>
